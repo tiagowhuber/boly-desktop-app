90beca2da7a9f7ab7c17b3ae0516ce37a6df82f6
tiagowhuber <thuber2018@udec.cl>
2025-05-12 19:59:05 -0400

    version number update

--- src/renderer/src/vite-env.d.ts
+++ src/renderer/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_GOOGLE_CLIENT_ID: string;
   readonly VITE_APP_API_URL: string;
   readonly VITE_S3_BASE_URL: string;
+  readonly GH_TOKEN: string;
 }
 
 interface ImportMeta {
