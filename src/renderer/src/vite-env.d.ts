fa44704c78bc2d33f629d84b671e425b2214ed4b
tiagowhuber <thuber2018@udec.cl>
2026-06-17 01:19:06 -0400

    type fixes

--- src/renderer/src/vite-env.d.ts
+++ src/renderer/src/vite-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<object, object, any>
+  export default component
+}
+
 interface ImportMetaEnv {
   readonly VITE_APP_GOOGLE_CLIENT_ID: string
   readonly VITE_APP_API_URL: string
